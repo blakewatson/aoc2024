a0811b6fa4d58c7ddcaee9abdffe40c2635b477d
Blake Watson <blake@blakewatson.com>
2024-12-02 21:37:40 -0600

    day two

---
+++ 02a.js
@@ -0,0 +1,22 @@
+import data from './02-input.js';
+
+const reports = data
+  .split('\n')
+  .map((line) => line.split(' ').map((_) => parseInt(_)));
+
+const safeReports = reports.filter((levels) => {
+  const benchmarkDelta = levels[0] - levels[1];
+  const benchmarkSign = Math.sign(benchmarkDelta);
+
+  for (let i = 0; i < levels.length - 1; i++) {
+    const delta = levels[i] - levels[i + 1];
+
+    if (!delta || Math.sign(delta) !== benchmarkSign || Math.abs(delta) > 3) {
+      return false;
+    }
+  }
+
+  return true;
+});
+
+console.log(safeReports.length);
---
+++ 02b.js
@@ -0,0 +1,40 @@
+import data from './02-input.js';
+
+const reports = data
+  .split('\n')
+  .map((line) => line.split(' ').map((_) => parseInt(_)));
+
+const isSafeReport = (levels) => {
+  const benchmarkDelta = levels[0] - levels[1];
+  const benchmarkSign = Math.sign(benchmarkDelta);
+
+  for (let i = 0; i < levels.length - 1; i++) {
+    const delta = levels[i] - levels[i + 1];
+
+    if (!delta || Math.sign(delta) !== benchmarkSign || Math.abs(delta) > 3) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const safeReports = reports.filter((levels) => {
+  let isSafe = isSafeReport(levels);
+
+  if (isSafe) {
+    return true;
+  }
+
+  for (let i = 0; i < levels.length; i++) {
+    const isSafe = isSafeReport([...levels].toSpliced(i, 1));
+
+    if (isSafe) {
+      return true;
+    }
+  }
+
+  return false;
+});
+
+console.log(safeReports.length);
