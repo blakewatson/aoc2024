d12f42590d41a37b3efd5f01926b911780de8850
Blake Watson <blake@blakewatson.com>
2024-12-07 17:36:12 -0600

    day 6 part 1

---
+++ day-06/a.js
@@ -0,0 +1,93 @@
+const { readFileSync } = require('fs');
+
+const data = readFileSync(`${__dirname}/input.txt`, 'utf-8');
+
+const map = data.split('\n').map((r) => r.split(''));
+const mapHistory = map.map((r) => [...r]);
+
+let dir = '^';
+let done = false;
+
+/** @type {number[]} */
+let xy = map.reduce((xy, row, y) => {
+  const x = row.indexOf('^');
+
+  if (x == -1) {
+    return xy;
+  }
+
+  return [x, y];
+}, []);
+
+while (!done) {
+  const [x, y] = xy;
+
+  mapHistory[y][x] = 'X';
+
+  const [x2, y2] = getNextPosition(x, y, dir);
+
+  if (!map[y2]?.[x2]) {
+    done = true;
+    continue;
+  }
+
+  if (map[y2][x2] === '#') {
+    dir = turn(dir);
+    continue;
+  }
+
+  xy = [x2, y2];
+}
+
+console.log(countPositions());
+
+function turn(dir) {
+  const directions = '^>v<';
+  return directions[(directions.indexOf(dir) + 1) % 4];
+}
+
+function getNextPosition(x, y, dir) {
+  if (dir === '^') {
+    return [x, y - 1];
+  } else if (dir === 'v') {
+    return [x, y + 1];
+  } else if (dir === '<') {
+    return [x - 1, y];
+  } else {
+    return [x + 1, y];
+  }
+}
+
+function countPositions() {
+  return mapHistory.reduce((total, row, y) => {
+    return (
+      total +
+      row.reduce((t, cell) => {
+        if (cell === 'X') {
+          return t + 1;
+        }
+        return t;
+      }, 0)
+    );
+  }, 0);
+}
+
+// function go(xy, dir = '^') {
+//   sanity++;
+//   console.log(sanity);
+//   const [x, y] = xy;
+
+//   mapHistory[y][x] = 'X';
+
+//   const [x2, y2] = getNextPosition(x, y, dir);
+
+//   if (!map[y2]?.[x2]) {
+//     return countPositions();
+//   }
+
+//   if (map[y2][x2] === '#') {
+//     return go(xy, turn(dir));
+//   }
+
+//   return go([x2, y2], dir);
+// }
